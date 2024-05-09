f838247edd1e9ca1edf2ffdb60cf5e3969819566
SameerDhumal <samdhumal25@gmail.com>
2024-05-09 16:48:52 +0530

    added setup.py,requirement.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import  setup,  find_packages
+from setuptools import find_packages,   setup
 from typing import List
- 
-def req()
-    x = list[str] = []
-    return x
+
+def get_req() -> List[str]:
+    return []
 
 setup(
-    name= 'sensor',
-    version= '0.0.1',
+    name='sensor',
+    version='0.0.1',
     author='Sameer',
     author_email='samdhumal25@gmail.com',
-    packages= find_packages(),
-    install_requires = req
+    packages=find_packages(),
+    install_requires=get_req(),
 )
+
